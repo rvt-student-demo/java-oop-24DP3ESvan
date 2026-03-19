82d4b62c29fc63a641884cd06a2a2c7ef8a74b3a
24DP3ESvan <A240393ES@rvt.lv>
2026-03-19 10:03:12 +0200

    Studentu registracijas sistema

--- src/main/java/rvt/Regex.java
+++ src/main/java/rvt/Regex.java
@@ -14,6 +14,6 @@ public class Regex {
     }
 
     public static boolean validatePersonalCode(String code) {
-        return code.matches("^[0-9]{12}$");
+        return code.matches("^[0-9]{10,12}$");
     }
 }
