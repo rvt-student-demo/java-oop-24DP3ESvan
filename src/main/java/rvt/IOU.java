2af4cd6004296d0621f4992b007b0a93a27c19d4
24DP3ESvan <A240393ES@rvt.lv>
2026-05-12 11:19:21 +0300

    IOU

---
+++ src/main/java/rvt/IOU.java
@@ -0,0 +1,24 @@
+package rvt;
+
+import java.util.HashMap;
+
+public class IOU {
+	HashMap<String, Double> IOUMap = new HashMap<>();
+
+	public void setSum(String toWhom, double amount) {
+		IOUMap.put(toWhom, amount);
+	}
+
+	public double howMuchDoIOweTo(String toWhom) {
+		return IOUMap.getOrDefault(toWhom, 0.0);
+	}
+
+	public static void main(String[] args) {
+		IOU OweIOU = new IOU();
+		OweIOU.setSum("Arthur", 51.5);
+		OweIOU.setSum("Michael", 30);
+
+		System.out.println(OweIOU.howMuchDoIOweTo("Arthur"));
+		System.out.println(OweIOU.howMuchDoIOweTo("Michael"));
+	}
+}
