203c87b241303cdc62ac494a5fe513a57202a8b6
24DPESvan <A240393ES@rvt.lv>
2026-06-07 11:03:33 +0300

    Person And Subclasses

--- src/main/java/rvt/Person.java
+++ src/main/java/rvt/Person.java
@@ -23,7 +23,7 @@ public class Person {
     }
     public static void main(String[] args) {
         Person ada = new Person("Ada Lovelace", "24 Maddox St. London W1S 2QN");
-        Person esko = new Person("Esko Ukonen", "Mannerheimintie 15 00100 Helsinki");
+        Person esko = new Person("Esko Ukkonen", "Mannerheimintie 15 00100 Helsinki");
         System.out.println(ada);
         System.out.println(esko);
     }
